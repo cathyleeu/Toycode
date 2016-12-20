9465833e64e2e22628a9a41d40a9eb50e01b1633
cathyleeu <leeyu823@gmail.com>
2016-12-21 02:29:31 +0900

    get total price but its need refactoring!

--- ForSite/src/components/order/Cart.js
+++ ForSite/src/components/order/Cart.js
@@ -2,75 +2,79 @@ import React, { Component, PropTypes } from 'react'
 import AddedProduct from './AddedProduct'
 
 
-class Cart extends Component {
-  constructor(props) {
-    super(props)
-  }
-  render(){
-    const nodes = this.props.books !== undefined ? (
-          this.props.books.map(book =>
-            <AddedProduct
-              title={book.title}
-              price={book.price}
-              key={book.id}
-              name={book.id}
-              // controlFunc={this.props.controlFunc}
-              qutt={this.props.qutt}
-              toggleSelect={this.props.toggleSelect}
-              onAddToOrder={this.props.onAddToOrder}
-            />
-          )
-        ) : (
-          <em> 상품을 담아주세요. </em>
-        )
-    return(
-      <div>
-        <h3>주문서</h3>
-        <div>{nodes}</div>
-        {/* <div>{checkout}</div> */}
-        <hr />
-
-        {/* <p>총 가격: {total}</p> */}
-      </div>
-    )
-  }
-}
-
-
-// const Cart = (props) => {
-//   const {books, amount, onAddToOrder,controlFunc, qutt, requestInvoice, toggleSelect} = props
-//   const nodes = books !== undefined ? (
-//       books.map(book =>
-//         <AddedProduct
-//           title={book.title}
-//           price={book.price}
-//           key={book.id}
-//           id={book.id}
-//           controlFunc={controlFunc}
-//           qutt={qutt}
-//           toggleSelect={toggleSelect}
-//           onAddToOrder={onAddToOrder}
-//         />
-//       )
-//     ) : (
-//       <em> 상품을 담아주세요. </em>
-//     )
-//   const checkout = nodes.length > 0 ? (
-//     <div>
-//       <button
-//         onClick={requestInvoice}>주문하기</button>
-//     </div>
-//   ): false
-//   return (
-//     <div>
-//       <h3>주문서</h3>
-//       <div>{nodes}</div>
-//       <div>{checkout}</div>
-//       <hr />
+// class Cart extends Component {
+//   constructor(props) {
+//     super(props)
+//   }
+//   render(){
+//     const nodes = this.props.books !== undefined ? (
+//           this.props.books.map(book =>
+//             <AddedProduct
+//               title={book.title}
+//               price={book.price}
+//               key={book.id}
+//               name={book.id}
+//               // controlFunc={this.props.controlFunc}
+//               qutt={this.props.qutt}
+//               toggleSelect={this.props.toggleSelect}
+//               onAddToOrder={this.props.onAddToOrder}
+//             />
+//           )
+//         ) : (
+//           <em> 상품을 담아주세요. </em>
+//         )
+//     return(
+//       <div>
+//         <h3>주문서</h3>
+//         <div>{nodes}</div>
+//         {/* <div>{checkout}</div> */}
+//         <hr />
 //
-//       {/* <p>총 가격: {total}</p> */}
-//     </div>
-//   )
+//         {/* <p>총 가격: {total}</p> */}
+//       </div>
+//     )
+//   }
 // }
+
+// !== undefined
+const Cart = ({books, selected, toggleSelect}) => {
+  const nodes = books.length > 0 ? (
+      books.map(book =>
+        <AddedProduct
+          title={book.title}
+          price={book.price}
+          key={book.id}
+          id={book.id}
+          toggleSelect={toggleSelect}
+        />
+      )
+    ) : (
+      <em> 상품을 담아주세요. </em>
+    )
+  const each = selected.length > 0 ? (
+    selected.map(each => (
+      <div key={each.id} value={each.total}>
+        <p>{each.title} : {each.total}</p>
+      </div>
+    ))
+  ): <em> 총가격 </em>
+  var all = []
+  const total = selected.length > 0 ? (
+    selected.map(each => {
+      all.push(each.total)
+    })
+  ) : false
+  const allTotal = all.length > 0 ? all.reduce((a,b) => a+b) : false
+  return (
+    <div>
+      <h3>주문서</h3>
+      <div>{nodes}</div>
+      <hr />
+
+      <div>{each}</div>
+      <div>{allTotal}</div>
+    </div>
+  )
+}
 
 export default Cart
--- ForSite/src/components/order/OrderForm.js
+++ ForSite/src/components/order/OrderForm.js
@@ -1,81 +1,29 @@
 import React, { Component, PropTypes } from 'react'
 import { connect } from 'react-redux'
-import { getTotal, getCartProducts } from '../../reducers'
+import { getCartProducts, getAddedCart } from '../../reducers'
 import { requestInvoice, requestQuantity, toggleSelect } from '../../actions/order'
 import Cart from './Cart'
 import {bindActionCreators} from 'redux'
 
-// import AddedProduct from './AddedProduct'
-
-
-class OrderForm extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-
-    seletedId: undefined
-    }
-    this.handleChange = this.handleChange.bind(this)
-  }
-  handleChange(e){
-    this.setState({
-      selectedId: e.target.name
-    }, () => console.log(parseInt(this.state.orderQuantity)))
-  }
-  //this.props.requestQuantity(parseInt(this.state.selectedId),parseInt(this.state.orderQuantity))
-  render(){
-    const props = this.props
-    return(
-        <Cart
-          books={props.books}
-          qutt={this.state.orderQuantity}
-          // controlFunc={this.handleChange}
-          selectedId={this.state.selectedId}
-          requestInvoice={() => { props.requestInvoice(parseInt(this.state.selectedId), parseInt(this.state.orderQuantity))}}
-          toggleSelect={props.toggleSelect}
-          
-         />
-    )
-  }
-}
-
-// const OrderForm = ({ books, total, amount }) => (
-//   <Cart
-//     books={books}
-//     // total={total}
-//     amount={amount}
-//     onAddToOrder={() => { addToOrder(books.id)}}
-//    />
 //
-// )
-
-
-// {books !== undefined ? books.map(book =>
-//   <CartList
-//     key={book.id}
-//     book={book}
-//     amount={amount}
-//     onAddToOrder={() => { addToOrder(book.id) }}
-//
-//   />
-// ): <div>상품을 담아주세요!</div>}
-
-
+ const OrderForm = ({books, toggleSelect, selected}) => (
+   <Cart
+    books={books}
+    toggleSelect={toggleSelect}
+    selected={selected}
+   />
+)
 
 function mapStateToProps(state){
   return {
     books: getCartProducts(state),
     selected: getAddedCart(state)
-    // total: getTotal(state),
-    // TODO: total도 뿌려줘야함.
   }
 }
 
 function mapDispatchToProps(dispatch){
   return bindActionCreators({
-    toggleSelect:toggleSelect,
-    requestQuantity:requestQuantity,
-    requestInvoice:requestInvoice
+    toggleSelect:toggleSelect
   }, dispatch)
 }
 
