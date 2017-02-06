c99239b73eec39c9ebf1cb5656d2c8534f03ec17
cathyleeu <leeyu823@gmail.com>
2017-02-06 15:44:43 +0900

    delete

--- mini-erp-server/config.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  secret: 'argrgrwg039495963'
-}
--- mini-erp-server/index.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-require('babel-plugin-transform-es2015-modules-commonjs')
-require('babel-plugin-transform-class-properties')
-require('babel-plugin-transform-object-rest-spread')
-require('babel-plugin-syntax-trailing-function-commas')
-require('./app.js');
