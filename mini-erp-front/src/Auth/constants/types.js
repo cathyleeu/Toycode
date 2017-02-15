ba0666439a96f92abef91be5142555f741018694
cathyleeu <leeyu823@gmail.com>
2017-02-15 16:31:14 +0900

    에러 메시지 수정, 주소창 수정

--- mini-erp-front/src/Auth/constants/types.js
+++ mini-erp-front/src/Auth/constants/types.js
@@ -1,7 +1,6 @@
 // user 인증
 export const AUTH_USER = 'AUTH_USER'
 export const UNAUTH_USER = 'UNAUTH_USER'
-export const AUTH_ERROR = 'AUTH_ERROR'
 export const STATUS_ON_LOGIN = 'STATUS_ON_LOGIN'
 
 export const REGISTERED_STATUS = 'REGISTERED_STATUS'
@@ -9,3 +8,12 @@ export const REGISTER_STATUS = 'REGISTER_STATUS'
 
 
 export const INITIAL_KINDER = 'INITIAL_KINDER'  //처음 시작할 때 DB의 kinder불러옴
+
+
+export const EMAIL_ERROR = 'EMAIL_ERROR'
+export const LOGIN_ERROR = 'LOGIN_ERROR'
+export const PASSWORD_ERROR = 'PASSWORD_ERROR'
+export const AUTH_ERROR = 'AUTH_ERROR'
+export const BIZ_ERROR = 'BIZ_ERROR'
+export const ADDR_ERROR = 'ADDR_ERROR'
+export const CODE_ERROR = 'CODE_ERROR'
