3b9ced31a01917ff2e4cc2aced82e68dd3f4ffbe
cathyleeu <leeyu823@gmail.com>
2017-03-02 17:25:20 +0900

    login

--- mini-erp-front/src/IssuedLogin/components/IssuedClasses.js
+++ mini-erp-front/src/IssuedLogin/components/IssuedClasses.js
@@ -29,15 +29,22 @@ function getCode( bId , kId ) {
 const IssuedClasses = (props) => {
   const { kinders, branch } = props.loginInfo,
           code = getCode(branch.name, kinders[0].name, "201703");
+          console.log(kinders[0].kinderClasses);
   return(
     <div>
       <h5>로그인 발급을 위해 각 반의 레벨을 꼭 기입해 주세요.</h5>
       원별 접속 주소: <a href={`https://toycode.org/code/${code}`} target="_blank">toycode.org/code/{code}</a>
-      {kinders[0].kinderClasses.map((kids, i) => (
-        <div  key={i} style={{margin: '1em'}}>
-          <IssuedClass code={code} kinderName={kinders[0].name} kclassName={kids.className} level={kids.level}/>
+      { kinders[0].kinderClasses.length > 0 ? (
+        kinders[0].kinderClasses.map((kids, i) => (
+          <div key={i} style={{margin: '1em'}}>
+            <IssuedClass code={code} kinderName={kinders[0].name} kclassName={kids.className} level={kids.level} disabled={kids.level === '' && 'disabled'}/>
+          </div>
+        ))
+      ) : (
+        <div>
+          마이페이지에서 원과 레벨을 입력해주세요.
         </div>
-      ))}
+      )}
     </div>
   )
 }
