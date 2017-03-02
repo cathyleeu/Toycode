432f9d4d59ba2187f7cf915c8538b6438f968383
cathyleeu <leeyu823@gmail.com>
2017-03-02 10:44:39 +0900

    로그인 발급 버그 수정

--- mini-erp-front/src/IssuedLogin/components/IssuedClass.js
+++ mini-erp-front/src/IssuedLogin/components/IssuedClass.js
@@ -1,55 +1,39 @@
-import React from 'react';
+import React from 'react'
 
-function getCode( bId , kId , date ){
-  let sum = 0;
-  if(date.length === 6) {
-    date += "01";
-  }
-  sum += bId.charCodeAt(0) * 17;
-  sum += bId.charCodeAt(1) * 13;
-  if(kId.slice(0, 2) === "러닝") {
-    if(kId.slice(2, 4) !== "서초") {
-      sum += kId.charCodeAt(2) * 13;
-      sum += kId.charCodeAt(3) * 29;
-    }
-  }
-  if(kId.slice(0, 2) === "이화") {
-    sum += kId.charCodeAt(2) * 13;
-    sum += kId.charCodeAt(3) * 29;
-  }
-  sum += kId.charCodeAt(0) * 11;
-  sum += kId.slice(-1).charCodeAt(0) * 19;
-  sum += kId.slice(parseInt(kId / 2, 10)).charCodeAt(0) * 7;
-  return sum.toString(16).slice(1);
-}
 
-const IssuedClass = ({loginInfo}) => {
-  const { kinders, branch } = loginInfo,
-        code = getCode(branch.name, kinders[0].name, "201703");
+const IssuedClass = ({ kinderName, kclassName, code, level, disable }) => (
+  <div>
+    {kinderName} - {kclassName}
+    <form action="https://toycode.org/issue" method="POST" target="_blank">
+      <input type="hidden" name="code" value={code} />
+      <input type="hidden" name="school" value={kinderName} />
+      <input type="hidden" name="className" value={kclassName} />
+      <input type="hidden" name="yearmonth" value="201703" />
+      <input type="hidden" name="level" value={level} />
+      <input type="submit" value="로그인 스티커 발급" disabled={disable}/>
+    </form>
+  </div>
+)
 
-  return(
-    <div>
-      원별 접속 주소: <a href={`https://toycode.org/code/${code}`} target="_blank">toycode.org/code/{code}</a>
-      {kinders && kinders[0].kinderClasses.map((kids, i) => (
-        <div key={i} style={{margin: '1em'}}>
-          {kinders[0].name} - {kids.className}
-          <form action="https://toycode.org/issue" method="POST" target="_blank">
-            <input type="hidden" name="code" value={code} />
-            <input type="hidden" name="school" value={kinders[0].name} />
-            <input type="hidden" name="className" value={kids.className} />
-            <input type="hidden" name="yearmonth" value="201703" />
-            <input type="hidden" name="level" value={kids.level} />
-            <input type="submit" value="로그인 스티커 발급" />
-          </form>
-        </div>
-      ))}
-    </div>
-)}
+
+// class IssuedClass extends Component {
+//
+//   render(){
+//     const { kinderName, kclassName, code, level, disable } = this.props;
+//     return(
+//       <div>
+//         {kinderName} - {kclassName}
+//         <form action="https://toycode.org/issue" method="POST" target="_blank">
+//           <input type="hidden" name="code" value={code} />
+//           <input type="hidden" name="school" value={kinderName} />
+//           <input type="hidden" name="className" value={kclassName} />
+//           <input type="hidden" name="yearmonth" value="201703" />
+//           <input type="hidden" name="level" value={level} />
+//           <input type="submit" value="로그인 스티커 발급" disabled={disable}/>
+//         </form>
+//       </div>
+//     )
+//   }
+// }
 
 export default IssuedClass;
-
-/* <form action="https://toycode.org/issue" method="POST" target="_blank"> */
-
-/* <button>원아수 수정</button>
-<button>ID 설정하기</button>
-<button>로그인 발급 받기</button> */
---
+++ mini-erp-front/src/IssuedLogin/components/IssuedClasses.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import IssuedClass from './IssuedClass'
+
+function getCode( bId , kId , date ){
+  let sum = 0;
+  if(date.length === 6) {
+    date += "01";
+  }
+  sum += bId.charCodeAt(0) * 17;
+  sum += bId.charCodeAt(1) * 13;
+  if(kId.slice(0, 2) === "러닝") {
+    if(kId.slice(2, 4) !== "서초") {
+      sum += kId.charCodeAt(2) * 13;
+      sum += kId.charCodeAt(3) * 29;
+    }
+  }
+  if(kId.slice(0, 2) === "이화") {
+    sum += kId.charCodeAt(2) * 13;
+    sum += kId.charCodeAt(3) * 29;
+  }
+  sum += kId.charCodeAt(0) * 11;
+  sum += kId.slice(-1).charCodeAt(0) * 19;
+  sum += kId.slice(parseInt(kId / 2, 10)).charCodeAt(0) * 7;
+  return sum.toString(16).slice(1);
+}
+
+
+const IssuedClasses = (props) => {
+  const { kinders, branch } = props.loginInfo,
+          code = getCode(branch.name, kinders[0].name, "201703");
+  return(
+    <div>
+      <h5>로그인 발급을 위해 각 반의 레벨을 꼭 기입해 주세요.</h5>
+      원별 접속 주소: <a href={`https://toycode.org/code/${code}`} target="_blank">toycode.org/code/{code}</a>
+      {kinders[0].kinderClasses.map((kids, i) => (
+        <div  key={i} style={{margin: '1em'}}>
+          <IssuedClass code={code} kinderName={kinders[0].name} kclassName={kids.className} level={kids.level} disable={(kids.level === 'none') ? 'disabled' : '' }/>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+// class IssuedClasses extends Component {
+//
+//   render(){
+//     const { kinders, branch } = this.props.loginInfo,
+//             code = getCode(branch.name, kinders[0].name, "201703");
+//     return(
+//       <div>
+//         원별 접속 주소: <a href={`https://toycode.org/code/${code}`} target="_blank">toycode.org/code/{code}</a>
+//         {kinders[0].kinderClasses.map((kids, i) => (
+//           <div  key={i} style={{margin: '1em'}}>
+//             <IssuedClass code={code} kinderName={kinders[0].name} kclassName={kids.className} level={kids.level} disable={(kids.level === 'none') ? 'disabled' : '' }/>
+//           </div>
+//         ))}
+//       </div>
+//     )
+//   }
+// }
+
+
+export default IssuedClasses;
+
+/* <form action="https://toycode.org/issue" method="POST" target="_blank"> */
+
+/* <button>원아수 수정</button>
+<button>ID 설정하기</button>
+<button>로그인 발급 받기</button> */
