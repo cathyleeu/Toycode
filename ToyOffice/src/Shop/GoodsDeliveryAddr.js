cef4a779acc2559b7e579c861bbc8bc8947da197
cathyleeu <leeyu823@gmail.com>
2017-04-21 17:34:26 +0900

    ui 수정

--- ToyOffice/src/Shop/GoodsDeliveryAddr.js
+++ ToyOffice/src/Shop/GoodsDeliveryAddr.js
@@ -1,32 +1,86 @@
 import React, {Component} from 'react'
-import {TextField, RaisedButton} from 'material-ui';
 
 class GoodsDeliveryAddr extends Component{
   state = {
-  //   rqcontent: '',
+    rqcontent: '',
 	// 	location: '',
-  //   phone: acct.A_phone || '',
+    phone: '',//acct.A_phone || '',
 		zipNo: '',
-	// 	roadAddr: address.roadAddr|| '',
-	// 	detailAddr: address.detailAddr|| '',
-  //   recipient: acct.A_manager || '',
+		roadAddr: '', //address.roadAddr|| '',
+		detailAddr: '',//address.detailAddr|| '',
+    recipient: '' //acct.A_manager || '',
   }
   render(){
     return(
-      <div className="Goods_Delivery_Addr">
-        <div className="Delivery-Addr-Row">
-          <TextField
-            hintText="주소검색으로 우편번호 선택하세요"
-            // floatingLabelText="우편번호"
-            type="zipNo"
-            name="zipNo"
-            value={this.state.zipNo}
-            disabled={true}
-            // onChange={this.handleChange}
-            // errorText={this.state.err}
-          />
-          <RaisedButton label="검색"/>
-          <RaisedButton label="주소록"/>
+      <div className="Goods-Delivery-Addr">
+        <div className="Delivery-Addr-Col">
+          <div className="Delivery-Addr-Row SpaceBtw">
+            <label>
+            우편번호
+            <input
+              className="Delivery-input"
+              placeholder='우편번호'
+              type='number'
+              name="zipNo"
+              value={this.state.zipNo}
+              readOnly/>
+            </label>
+            <button className="Delivery-Addr-btn">주소 검색</button>
+            <button className="Delivery-Addr-btn">주소록</button>
+          </div>
+          <div>
+            <label>배송처
+            <input
+              className="Delivery-input"
+              placeholder='배송지'
+              type='text'
+              name="roadAddr"
+              value={this.state.roadAddr}
+            />
+            </label>
+            <label>배송지 상세주소
+            <input
+              className="Delivery-input"
+              placeholder='배송지 상세주소'
+              type='text'
+              name="detailAddr"
+              value={this.state.detailAddr}
+            />
+            </label>
+          </div>
+          <div className="Delivery-Addr-Row SpaceBtw">
+            <label>
+              수령인
+            <input
+              className="Delivery-input half"
+              placeholder='수령인'
+              type='text'
+              name="recipient"
+              value={this.state.recipient}
+            />
+            </label>
+            <label>연락처
+            <input
+              className="Delivery-input"
+              placeholder='연락처'
+              type='text'
+              name="phone"
+              value={this.state.phone}
+            />
+            </label>
+          </div>
+          <div>
+            <label>
+              요구사항
+            <input
+              className="Delivery-input"
+              placeholder='요구사항'
+              type='text'
+              name="rqcontent"
+              value={this.state.rqcontent}
+            />
+            </label>
+          </div>
         </div>
       </div>
     )
