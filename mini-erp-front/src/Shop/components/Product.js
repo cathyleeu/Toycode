fe4d9b186d2bc54318c53dd1c39a8f401f97c61a
cathyleeu <leeyu823@gmail.com>
2018-01-24 11:56:34 +0900

    겨울특별호

--- mini-erp-front/src/Shop/components/Product.js
+++ mini-erp-front/src/Shop/components/Product.js
@@ -3,7 +3,16 @@ import React, {PureComponent} from 'react'
 
 class Product extends PureComponent {
   render(){
-    const {imgcode, className, title, volume, level} = this.props;
+    let {imgcode, className, title, volume, level} = this.props;
+    let hookName = {
+      "A특별1" : "ASP1",
+      "B특별1" : "BSP1",
+      "C특별1" : "CSP1"
+    }
+    imgcode = hookName[imgcode] || imgcode ;
+    if(volume === "SP1") {
+      volume = "특별(상)"
+    }
     let bookCover = require(`../../../public/img/${imgcode}.png`)
     return(
       <div className={className}>
