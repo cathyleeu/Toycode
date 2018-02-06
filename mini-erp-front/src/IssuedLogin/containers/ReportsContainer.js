58a0f33a91a2648aa76944229341e48647a59a18
이유경 <leeyu823@gmail.com>
2018-02-06 06:30:05 +0000

    Renew issue03

--- mini-erp-front/src/IssuedLogin/containers/ReportsContainer.js
+++ mini-erp-front/src/IssuedLogin/containers/ReportsContainer.js
@@ -56,7 +56,7 @@ const ReportCard = ({
   studentName,
   studentValue,
   totalAverage,
-  classAverage
+  classAverage,
 }) => (
   <div className="report_card">
     <div className="report_card_header">
@@ -90,7 +90,25 @@ class ReportByStudent extends PureComponent {
         "A": "우수한 학생입니다.",
         "B": "보통 학생입니다.",
         "C": "미흡한 학생입니다."
-      }
+      },
+      reportsResults: {},
+      loaded: false
+    }
+  }
+  componentWillMount(){
+    let { classId, userId, level } = this.props;
+    let chapter = `${level}0_w0_`
+    chapter = chapter.toLocaleLowerCase()
+    this.props.requestedReports(classId, userId, chapter)
+  }
+  componentWillReceiveProps(newProps){
+    if(newProps.reportsResults !== this.props.reportsResults) {
+     this.setState({
+       reportsResults: {
+         ...newProps.reportsResults
+       },
+       loaded: true
+     })
     }
   }
   handleChange = (e) => {
@@ -104,16 +122,25 @@ class ReportByStudent extends PureComponent {
       [e.target.name] : e.target.value
     })
   }
+  renderReportCards(target) {
+    return (
+      <ReportCard
+       studentName={this.props.name}
+       {...this.state.reportsResults[target]}
+     />
+    )
+  }
   render(){
-    const data = [
-      {name: '1권', 미완료: 5, 블럭초과: 40, 완료: 240, 반평균블록: 80, 사용한블록: 140},
-      {name: '2권', 미완료: 6, 블럭초과: 98, 완료: 210, 반평균블록: 96, 사용한블록: 150},
-      {name: '3권', 미완료: 13, 블럭초과: 80, 완료: 220, 반평균블록: 98, 사용한블록: 89},
-      {name: '4권', 미완료: 14, 블럭초과: 29, 완료: 200, 반평균블록: 120, 사용한블록: 128},
-      {name: '5권', 미완료: 5, 블럭초과: 18, 완료: 181, 반평균블록: 110, 사용한블록: 100},
-      {name: '특별상권', 미완료: 14, 블럭초과: 20, 완료: 250, 반평균블록: 180, 사용한블록: 170}
-    ];
+    if(!this.state.loaded) {
+      return (
+        <section className="print">
+          <div>로딩중</div>
+        </section>
+      )
+    }
+
     let { school, className, level, name } = this.props;
+
     return (
       <section className="print">
         <div className="reports-top">
@@ -123,26 +150,25 @@ class ReportByStudent extends PureComponent {
         <div className="reports-body">
           <div className="reports-chart-cont">
             <ReportsSubHeader name="키즈씽킹 학습 현황 그래프"/>
-            <ComposedChart width={500} height={220} data={data}
+            <ComposedChart width={500} height={220} data={this.state.reportsResults.chapterAves}
                 margin={{top: 20, right: 20, bottom: 20, left: 20}}>
               <XAxis dataKey="name"/>
               <YAxis />
               <Tooltip/>
               <Legend/>
               <CartesianGrid stroke='#f5f5f5'/>
-              <Bar dataKey="블럭초과" stackId="a" fill="#A84546" />
-              <Bar dataKey="완료" stackId="a" fill="#82ca9d" />
-              <Bar dataKey="미완료" fill="#ffc658"/>
-              <Line type='monotone' dataKey='반평균블록' stroke='#ff7300'/>
-              <Line type='monotone' dataKey='사용한블록' stroke='#8884d8'/>
+              <Bar dataKey="학생블럭수" stackId="a" fill="#82ca9d" />
+              <Bar dataKey="반평균블럭" fill="#ffc658"/>
+              <Line type='monotone' dataKey='학생시도횟수' stroke='#ff7300'/>
+              <Line type='monotone' dataKey='반평균시도횟수' stroke='#8884d8'/>
            </ComposedChart>
          </div>
          <ReportsGuideCont>
            <DescribeName name="막대 그래프">
-             <li>미완료 (주황색)</li>
-             <li>아직풀지못한문제수</li>
-             <li>블록초과(적색)</li>
-             <li>기준보다 많은 블록을 사용</li>
+             {/* <li>미완료 (주황색)</li>
+             <li>아직풀지못한문제수</li> */}
+             {/* <li>블록초과(적색)</li>
+             <li>기준보다 많은 블록을 사용</li> */}
              <li>완료(녹색)</li>
              <li>최소의 블록을 사용한 경우</li>
            </DescribeName>
@@ -157,34 +183,12 @@ class ReportByStudent extends PureComponent {
         <div className="reports-body">
          <div className="reports-chart-cont">
            <ReportsSubHeader name="키즈씽킹 학습 이력 통계"/>
-           <ReportCard
-            reportTitle="사용 블럭 수"
-            studentName={name}
-            studentValue={10}
-            totalAverage={8.5}
-            classAverage={9.5}
-          />
-          <ReportCard
-            reportTitle="시도 횟수"
-            studentName={name}
-            studentValue={3}
-            totalAverage={4.2}
-            classAverage={2.3}
-          />
-          <ReportCard
-            reportTitle="풀이 시간"
-            studentName={name}
-            studentValue={"02:22"}
-            totalAverage={"03:05"}
-            classAverage={"02:25"}
-          />
-          <ReportCard
-            reportTitle="점수"
-            studentName={name}
-            studentValue={11}
-            totalAverage={12.5}
-            classAverage={7.5}
-          />
+
+           {this.renderReportCards('block')}
+           {this.renderReportCards('failed')}
+           {this.renderReportCards('duration')}
+           {this.renderReportCards('score')}
+
          </div>
           <ReportsGuideCont>
             <DescribeName name="사용 블럭 수" decs="문제를 풀기위해 사용한 평균 블록 수 입니다. 적은 블록을 사용할 수록 좋아요!"/>
@@ -229,15 +233,20 @@ class ReportsContainer extends Component {
       obj[r.split("=")[0]] = r.split("=")[1]
     })
     obj.school = obj.school.split("+").join('')
-    let fakeData = require('./fakeData').fakeData[obj.academyId]
-    let data =  fakeData ? fakeData[obj.classId] : null
+
     this.setState({
-      ...obj,
-      data
+      ...obj
     })
   }
   renderReportsByStudent(name, id) {
-    return <ReportByStudent key={id} name={name} school={this.state.school} className={this.state.className} level={this.state.level}/>
+    let { students, ...stateRest } = this.state;
+    return <ReportByStudent
+              key={id} name={name}
+              userId={id}
+              reportsResults={this.props.reportsResults[id]}
+              requestedReports={this.props.requestedReports}
+              {...stateRest}
+            />
   }
   render(){
     return (
@@ -263,10 +272,7 @@ class ReportsContainer extends Component {
 
 
 const mapStateToProps = (state) => ({
-  // recordedKinders: state.auth.user.kinders,
-  // loginInfo: state.issuedLogin,
-  // studentsNames: state.issuedLogin,
-  // customerType: state.auth.user.customerType
+  reportsResults: state.issuedLogin.reports
 })
 
 export default connect(mapStateToProps, actions)(ReportsContainer)
