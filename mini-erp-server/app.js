2c9d16d9a44a53917b3555a474dcc6091967a7f5
이정석 <nemesisler@gmail.com>
2020-04-20 05:32:01 +0000

    Revert "Merge branch 'patch-1' into 'master'"
    This reverts commit 471009ae07497b3da9adb023e9ad9577f434a519.

--- mini-erp-server/app.js
+++ mini-erp-server/app.js
@@ -47,11 +47,5 @@ app.use(cacheControl({
 app
   .use(router.routes())
   .use(router.allowedMethods())
-  
-app.all('/*', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next();
-});
 
 app.listen(3090);
